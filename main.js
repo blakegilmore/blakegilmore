27b568f407c67eca47b509c4c0e2aae46623c0b8
Blake <mblakegilmore@gmail.com>
2026-09-14 14:24:08 -0500

    lowercase

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const siteHeader = document.createElement('header');
 const siteTitle = document.createElement('a');
 const siteFooter = document.createElement('footer');
+const isHomePage = document.body.classList.contains('home-page');
 
 siteHeader.className = 'site-header';
 siteTitle.href = 'index.html';
-siteTitle.textContent = 'Blakeworld';
+siteTitle.textContent = isHomePage ? 'blakeworld' : 'Blakeworld';
 siteHeader.append(siteTitle);
 
 siteFooter.className = 'site-footer';
-siteFooter.textContent = 'Unofficial fan/curation site. Not affiliated with the artists, people, or organizations featured.';
+siteFooter.textContent = isHomePage
+	? 'unofficial fan/curation site. not affiliated with the artists, people, or organizations featured.'
+	: 'Unofficial fan/curation site. Not affiliated with the artists, people, or organizations featured.';
 
 document.body.prepend(siteHeader);
 document.body.append(siteFooter);
